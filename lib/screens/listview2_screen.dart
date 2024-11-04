7e3ea0deddcb62dc6175409ea4eaa3003ca2e91e
Jose Oteros <oteros19@gmail.com>
2024-11-04 14:01:09 +0100

    listview2

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+
+final options = const['Targarian', 'Stark', 'Lanister', 'Baratheon'];
+   
+  const Listview2Screen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView tipo 2'),
+        backgroundColor: Colors.amber,
+      ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => Text(options[index]), 
+        separatorBuilder: (context, index) => const Divider(), 
+        itemCount: options.length)
+    );
+  }
+}
