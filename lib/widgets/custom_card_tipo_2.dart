5d902ecb1b7e120babab8ce9b280332ae62929b8
Jose Oteros <oteros19@gmail.com>
2024-11-15 09:16:00 +0100

    assets

--- lib/widgets/custom_card_tipo_2.dart
+++ lib/widgets/custom_card_tipo_2.dart
@@ -5,10 +5,26 @@ class CustomCardTipo2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20)
+      ),
+      clipBehavior: Clip.antiAlias,
       child: Column(
         children: [
-          Image(image: NetworkImage('https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/08/peliculas-star-wars.jpg?tf=3840x')),
+          const FadeInImage(
+            image: NetworkImage('https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/08/peliculas-star-wars.jpg?tf=3840x'), 
+            placeholder: AssetImage('assets/carga.gif'),
+            width: double.infinity,
+            height: 260,
+            fit:BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 3000),
+            ),
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(top: 10, bottom: 10, right: 20),
+              child: const Text('Star Wars')
+              )
           ],
       ),
     );
