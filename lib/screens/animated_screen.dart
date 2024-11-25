1d918876fb4d5989e55768a143cebbeef919b4e2
Jose Oteros <oteros19@gmail.com>
2024-11-25 13:37:02 +0100

    set state

--- lib/screens/animated_screen.dart
+++ lib/screens/animated_screen.dart
@@ -1,8 +1,9 @@
+import 'dart:math';
+
 import 'package:fl_componentes/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedScreen extends StatefulWidget {
-   
   const AnimatedScreen({super.key});
 
   @override
@@ -17,19 +18,24 @@ class _AnimatedScreenState extends State<AnimatedScreen> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Animated Container')
-      ),
+      appBar: AppBar(title: const Text('Animated Container')),
       body: Center(
-         child: Container(
+        child: Container(
           height: _height,
           width: _width,
-          decoration: BoxDecoration(
-            color: AppTheme.primary,
-            borderRadius: _borderRadius
-          ),
-         ),
+          decoration: BoxDecoration(color: _color, borderRadius: _borderRadius),
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.play_arrow, size: 35),
+          onPressed: () {
+            setState(() {
+              _height = (Random().nextDouble() * 500) + 10;
+              _width = (Random().nextDouble() * 500) + 10;
+              _borderRadius = BorderRadius.circular(Random().nextDouble() * 50);
+              _color = Colors.primaries[Random().nextInt(Colors.primaries.length)];
+            });
+          }),
     );
   }
 }
