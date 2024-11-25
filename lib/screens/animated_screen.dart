737207068a95c12e51a189baca9267091b57b510
Jose Oteros <oteros19@gmail.com>
2024-11-25 12:26:25 +0100

    propiedades privadas

--- lib/screens/animated_screen.dart
+++ lib/screens/animated_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_componentes/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedScreen extends StatefulWidget {
@@ -9,14 +10,25 @@ class AnimatedScreen extends StatefulWidget {
 }
 
 class _AnimatedScreenState extends State<AnimatedScreen> {
-  @override
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.amber;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(10);
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Animated Container')
       ),
-      body: const Center(
-         child: Text('AnimatedScreen'),
+      body: Center(
+         child: Container(
+          height: _height,
+          width: _width,
+          decoration: BoxDecoration(
+            color: AppTheme.primary,
+            borderRadius: _borderRadius
+          ),
+         ),
       ),
     );
   }
